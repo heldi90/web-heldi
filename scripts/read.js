17cbfe78feba8cee753a64285c52495c616a3987
Heldi Arisandi <heldibisnis@gmail.com>
2021-12-22 19:22:48 +0800

    berhasil membuat control data

--- scripts/read.js
+++ scripts/read.js
@@ -18,14 +18,26 @@ async function showData(url = '', data = {}) {
 }
 
 var load = document.getElementById('loading');
-var page = document.getElementById('page');
+let halaman = document.getElementById('page');
 
-for (let i = 0; i < 2; i++) {
-  page.innerHTML += `<a id="page${i}" href="#">${i + 1}</a> |`;
-}
+// var dataindex = 10;
+var page = 0;
+var dataPage = [5];
+
+const pageData = (page, halaman) => {};
 
 showData('https://apiaja.herokuapp.com/api/posts', {}).then((data) => {
   let index = data.length;
+  for (let i = 0; i < index; i++) {
+    page++;
+    if (page > 5) {
+      dataPage.push(5);
+      page = 1;
+
+      // console.log(nilaidata)
+    }
+  }
+
   console.log(data);
   let table = document.getElementById('tbody');
   for (let i = 0; i < 5; i++) {
@@ -36,5 +48,59 @@ showData('https://apiaja.herokuapp.com/api/posts', {}).then((data) => {
                     <td>${data[i]['score']}</td>
                    </tr>`;
   }
+  let ini = document.getElementById('tr');
+  for (let i = 1; i - 1 < dataPage.length; i++) {
+    halaman.innerHTML += `<a id="page${i}" type="button" >${i}huu</a> |`;
+    console.log(`ini data saya :  ${i}`);
+  }
+
+  for (let i = 1; i - 1 < dataPage.length; i++) {
+    var elem = document.getElementById(`page${i}`);
+
+    if (elem) {
+      elem.addEventListener('click', function () {
+        let a = dataPage.slice(0, i).reduce((a, b) => a + b, 0);
+        table.innerHTML = '';
+        console.log(`ngeklick event ke - ${i}dan ${a}`);
+        // let table = document.getElementById('tbody');
+
+        for (let i = a - 5; i < 0 + a; i++) {
+          load.remove();
+          table.innerHTML += `<tr id="tr">
+                        <th scope="row">${i + 1}</th>
+                        <td>${data[i]['username']}</td>
+                        <td>${data[i]['score']}</td>
+                       </tr>`;
+        }
+      });
+    }
+  }
+  //   document.getElementById(`page${i}`).addEventListener('click', () => {
+  //     console.log(`ngeklick event ke - ${i}`);
+  //     let table = document.getElementById('tbody');
+  //     for (let i = a - 5; i < 0 + a; i++) {
+  //       load.remove();
+  //       table.innerHTML += `<tr>
+  //                       <th scope="row">${i + 1}</th>
+  //                       <td>${data[i]['username']}</td>
+  //                       <td>${data[i]['score']}</td>
+  //                      </tr>`;
+  //     }
+  //   });
+  // }
+
+  // First add all the new content:
+  // var html = halaman.innerHTML;
+  // for (var i = 1; i - 1 < dataPage.length; i++) {
+  //   html += `<a id="page${i}" href="#">${i}hahaha</a> |`;
+  //   let a = dataPage.slice(0, i).reduce((a, b) => a + b, 0);
+  //   console.log(`ini data saya : ${a} dan ${i}`);
+  // }
+  // halaman.innerHTMLL = html;
+
+  // Now bind the event handlers
+  // By using "let" instead of "var" the right value is retained
+  // in the handlers
+
   // JSON data parsed by `data.json()` call
 });
